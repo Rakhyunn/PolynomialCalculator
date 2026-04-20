e88f1c1b627528e145b381a6342710b3dc9327ed
Rakhyunn <krh0323@pukyong.ac.kr>
2026-04-20 16:58:12 +0900

    [feat] 곱셈 다항식 계산 구현

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,14 +1,35 @@
 package com.ll;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Calc {
     public static int run(String expression) {
         String[] expressionBits = expression.split(" ");
 
-        int result = Integer.parseInt(expressionBits[0]);
+        if(expression.contains("*")) {
+            for(int i = 0; i < expressionBits.length; i++){
+                if(expressionBits[i].equals("*")){
+                    int num1 = Integer.parseInt(expressionBits[i - 1]);
+                    int num2 = Integer.parseInt(expressionBits[i + 1]);
+                    expressionBits[i + 1] = String.valueOf(num1 * num2);
+                    expressionBits[i - 1] = "";
+                    expressionBits[i] = "";
+                }
+            }
+        }
 
-        for(int i = 1; i < expressionBits.length; i += 2){
-            String op = expressionBits[i];
-            int num = Integer.parseInt(expressionBits[i + 1]);
+        List<String> expressionList = Arrays.stream(expressionBits)
+                .filter(s -> !s.isBlank())
+                .toList();
+
+        int result = Integer.parseInt(expressionList.get(0));
+
+        if(expressionList.size() == 1) return result;
+
+        for(int i = 1; i < expressionList.size(); i += 2){
+            String op = expressionList.get(i);
+            int num = Integer.parseInt(expressionList.get(i + 1));
             if(op.equals("+")) result += num;
             else result -= num;
         }
