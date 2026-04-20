24db2e3158f50fe18f9e45d1e5641a90d89b6217
Rakhyunn <krh0323@pukyong.ac.kr>
2026-04-20 16:22:23 +0900

    [feat] 뺄셈 계산 구현

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,11 +2,12 @@ package com.ll;
 
 public class Calc {
     public static int run(String expression) {
-        String[] expressionBits = expression.split(" \\+ ");
+        String[] expressionBits = expression.split(" \\+ | - ");
 
         int num1 = Integer.parseInt(expressionBits[0]);
         int num2 = Integer.parseInt(expressionBits[1]);
 
-        return num1 + num2;
+        if(expression.contains("+")) return num1 + num2;
+        else return num1 - num2;
     }
 }
